6bc3349a1421abf875c715daeaf3957949ba0167
vdanmad <vdanmadeira@gmail.com>
2023-12-20 00:15:36 +0000

    fix 2

--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import ReactPlayer from 'react-player'
+import Image from 'next/image';
+import styles from './page.module.css';
+import ReactAudioPlayer from 'react-audio-player';
 
 export default function Home() {
   return (
     <main className={styles.main}>
 
       <div className={styles.center}>
-        <ReactPlayer url="http://xplayer.neohub.pt:1905/stream/swyh.mp3"/>
+        <ReactAudioPlayer
+		src="http://xplayer.neohub.pt:1905/stream/swyh.mp3"
+		autoPlay
+		controls
+	/>
       </div>
 
     </main>
